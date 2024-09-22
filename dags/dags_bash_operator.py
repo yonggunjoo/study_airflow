34304dbb89dee8249add2850324367601d5cb6bc
yonggun.joo <study.jyg@gmail.com>
2024-09-22 16:19:25 +0900

    bash_operator dag upload

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,27 @@
+
+from __future__ import annotations
+
+import datetime
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+# 분,시,일,월,요일
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    
+    bash_t1 >> bash_t2
